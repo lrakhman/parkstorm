45a96ffe3799ddfbfa022f6e27eba3167b660fbe
catherinev <catherinev@users.noreply.github.com>
2014-07-03 19:41:21 -0500

    add custom query methods to region model

--- app/models/region.rb
+++ app/models/region.rb
@@ -1,4 +1,30 @@
 class Region < ActiveRecord::Base
 	has_many :notifications
 	has_many :users, through: :notifications
+
+  def self.find_ward_section(lat, long)
+    query = "SELECT ward_secti FROM regions 
+    WHERE ST_DISTANCE_SPHERE(geom,
+    ST_MakePoint(#{lat}, #{long})) <= 0;"
+
+    Region.connection.execute(query).first
+  end
+
+  def sections_nearby(distance)
+    center = find_center
+    query = "SELECT geom FROM regions 
+    WHERE ST_DISTANCE_SPHERE(geom, \'#{center}\') <= #{distance} * 1609.34;"
+
+    result = []
+    Region.connection.execute(query).each {|thing| result << thing}
+    result
+  end
+
+
+  private
+  def find_center
+    query = "SELECT ST_AsText(ST_CollectionExtract(ST_GeomFromText(\'#{self.geom}\'),1));"
+
+    Region.connection.execute(query).first.first[1]
+  end
 end
