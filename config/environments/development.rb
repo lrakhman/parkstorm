23eb81ddb4cf9f777bd5b1cb041762ff20af363a
pdebelak <pdebelak@gmail.com>
2014-07-05 18:35:41 -0500

    Fix type in config files

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,7 +15,7 @@ Rails.application.configure do
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
-  config.cache_store = :dalli_store { expires_in 12.hours }
+  config.cache_store = :dalli_store { expires_in: 12.hours }
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
