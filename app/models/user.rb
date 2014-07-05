ac480b347633401bae57e1de34714a7e976bab64
lrakhman <lana.rakhman@gmail.com>
2014-07-04 19:56:50 -0500

    removed password_digest field in users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,6 @@ class User < ActiveRecord::Base
 	has_many :notifications
 	has_many :regions, through: :notifications
 
-	has_secure_password
-
-
 	#format for email should be changed, but good for now
 	validates :email, uniqueness: true, :format => /.+@.+\..+/ 
 
--- db/migrate/20140703233058_create_users.rb
+++ db/migrate/20140703233058_create_users.rb
@@ -2,7 +2,6 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
     	t.string :email, :null => false
-    	t.string :password_digest
     	t.string :firstname
     	t.string :lastname
 
