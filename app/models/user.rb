d3ddf6691291f57f4146d4b654875c4a0b11708b
lrakhman <lana.rakhman@gmail.com>
2014-07-04 19:32:13 -0500

    added authentications

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,14 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable
 	
 	has_many :notifications
 	has_many :regions, through: :notifications
 
 	has_secure_password
+
 
 	#format for email should be changed, but good for now
 	validates :email, uniqueness: true, :format => /.+@.+\..+/ 
