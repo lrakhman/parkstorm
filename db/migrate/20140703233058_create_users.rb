a4e63f553bf4c27d0a4628a4b9eb45d2fc1e5c1e
lrakhman <lana.rakhman@gmail.com>
2014-07-04 20:54:44 -0500

    making views for passwords, registrations, and sessions

--- db/migrate/20140703233058_create_users.rb
+++ db/migrate/20140703233058_create_users.rb
@@ -1,12 +1,10 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-    	t.string :email, :null => false
     	t.string :firstname
     	t.string :lastname
 
       t.timestamps
     end
-    add_index :users, :email, :unique => true
   end
 end
