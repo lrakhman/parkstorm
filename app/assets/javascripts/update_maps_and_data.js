d95675102e4d74d4bc9feb2e1324f63fb2f301b9
pdebelak <pdebelak@gmail.com>
2014-07-06 15:57:05 -0500

    refactored js and views plus regions controller

--- app/assets/javascripts/update_maps_and_data.js
+++ app/assets/javascripts/update_maps_and_data.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-
+  var active_map;
   findUser();
 
   $('#address_submit').on('click', function(event){
@@ -29,38 +29,37 @@ function findAddress(){
     var lat = results[0].geometry.location.k;
     var lng = results[0].geometry.location.B;
     data = {latitude: lat, longitude: lng};
-    console.log(data);
     updatePageFromAddress(data, address);
   });
 }
 
 function updatePage(data, location) {
+  postCurrentLocation(data, location);
+  
   $.post('/load_region', data, function(response){ 
     $('#active_map').append(response)
   });
-
-  $.post('/current_position', data, function(response){ 
-    $('.copy p:first-child').html('Next Street Cleaning<br>For ' + location + ' Is:<br>' + response.next_sweep);
-    var days = response.sweep_days;
-    str = "<h3>Next Cleaning: " + response.next_sweep + "</h3><h3>Street Cleaning Days</h3><ul>";
-    for (var i=0; i<days.length; i++) {
-      str += '<li>' + days[i] + '</li>';
-    }
-    $('.cleaning_col').html(str + '</ul>');
-  }, 'JSON');
 }
 
 function updatePageFromAddress(data, location) {
+  postCurrentLocation(data, location);
+
   $.post('/load_region_from_address', data, function(response){ 
     $('#active_map').replaceWith(response)
   });
+}
 
+function postCurrentLocation(data, location) {
   $.post('/current_position', data, function(response){ 
     $('.copy p:first-child').html('Next Street Cleaning<br>For ' + location + ' Is:<br>' + response.next_sweep);
-    var days = response.sweep_days;
     str = "<h3>Next Cleaning: " + response.next_sweep + "</h3><h3>Street Cleaning Days</h3><ul>";
-    for (var i=0; i<days.length; i++) {
-      str += '<li>' + days[i] + '</li>';
+    var days = response.sweep_days;
+    if (days.length > 0) {
+      for (var i=0; i<days.length; i++) {
+        str += '<li>' + days[i] + '</li>';
+      }
+    } else {
+      str += '<li>none</li>';
     }
     $('.cleaning_col').html(str + '</ul>');
   }, 'JSON');
