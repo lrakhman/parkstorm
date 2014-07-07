245eaa43908abcd4829fb25d7f04e0566dfd4237
Liz Kaplan <lkap44@gmail.com>
2014-07-06 19:51:25 -0500

    deleted some files, working on profile page. Need to get notifications fixed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,29 @@ class UsersController < ApplicationController
     NotificationMailer.sign_up_notification(@user).deliver
   end
 
+  def show
+    @user = User.find_by_id(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+
+    flash.notice = "Account Updated!"
+
+    redirect_to user_path(@user)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:fullname, :email)
+  end
+
 end
 
     # @regions = Region.all.pluck(:ward_secti)
