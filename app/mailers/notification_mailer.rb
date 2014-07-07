a58d6c24e4fd06c867a7148d81667601131a61ed
lrakhman <lana.rakhman@gmail.com>
2014-07-07 11:32:35 -0500

    changed notice to user in notification_mailer ;

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -11,8 +11,9 @@ class NotificationMailer < ActionMailer::Base
              # :unsub_link => unsub_email_link("123"))   
   end
 
-  def sweep_notification(notice)
-      mail(:to => notice.email,
+  def sweep_notification(user)
+      @user = user
+      mail(:to => user.email,
              # :ward => notice.region.ward,
              # :area => notice.region.sweep,
              :subject => "Sweeping Notification for Ward #{notice.region.ward_num}")
