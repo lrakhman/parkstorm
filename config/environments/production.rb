4da24d705f1505274a9e66f44af90957bea5d70d
lrakhman <lana.rakhman@gmail.com>
2014-07-07 11:00:58 -0500

    still working on email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,7 +10,7 @@ Rails.application.configure do
   config.cache_classes = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
 
   # Code is not reloaded between requests.
