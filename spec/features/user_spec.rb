b647bfb76693582d70061dabbf08a87e0a80289e
NatalieFrecka <nfrecka@gmail.com>
2014-07-07 20:16:56 -0500

    pulling from master

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -2,27 +2,22 @@ require 'rails_helper'
 
 feature 'User browsing the website' do
   context "on initial landing page" do
-    # xit "login button brings up modal" do
-    #   visit root_path
-    #   click_on "Log In"
-    #   sleep 1
-    #   expect(page).to have_css('#user_email')
-    # end
-
-    # xit "user is able to log in and see their user page" do
-    #   visit root_path
-    #   click_on "Log In"
-    #   User.create(email: "nfrecka@gmail.com", password: "caketime99")
-    #   fill_in '#user_email',   with: user.email
-    #   fill_in '#user_password', with: 'caketime99'
-    #   click_on "Log In"
-    #   expect(current_url).to eq()
-    # end
-
     it "street sweeping window updates" do
       visit root_path
-      expect(page).to have_content('The next street cleaning for your location is: _____________________')
+      sleep 3
+      expect(page).to have_content('The next street cleaning for your current location is: July 16')
     end
+
+    it "user is able to log in and see their user page" do
+      user = User.create(email: "nfrecka@gmail.com", password: "caketime99")
+      visit root_path
+      sleep 4
+      fill_in 'user[email]',   with: user.email
+      fill_in 'user[password]', with: 'caketime99'
+      click_on "Log In"
+      expect(current_url).to eq()
+    end
+
 
     
   end
