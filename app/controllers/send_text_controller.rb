abb81ab994936c17d1561b1a6880373dca0ffaa2
lrakhman <lana.rakhman@gmail.com>
2014-07-08 14:35:40 -0500

    added phone numbers to users and to notifications

--- app/controllers/send_text_controller.rb
+++ app/controllers/send_text_controller.rb
@@ -3,18 +3,29 @@ class SendTextController < ApplicationController
   end
  
   def send_text_message
-    number_to_send_to = params[:number_to_send_to]
+    number_to_send_to = params[:notification][:phone]
  
-    twilio_sid = "abc123"
-    twilio_token = "foobar"
-    twilio_phone_number = "6165555555"
+    twilio_sid = ENV["twilio_sid"]
+    twilio_token = ENV["twilio_token"]
+    twilio_phone_number = ENV["twilio_phone_number"]
  
-    @twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
+    @twilio_client = Twilio::REST::Client.new(twilio_sid, twilio_token)
  
+    if user_signed_in?
+      unless current_user.regions.include?(Region.find(session[:current_region_id]))
+        current_user.notifications.create!(region_id: session[:current_region_id], phone: current_user.phone)
+      end
+    else
+      if Notification.where(region_id: session[:current_region_id], phone: phone).empty?
+        Notification.create!(region_id: session[:current_region_id], phone: phone)
+      end
+    end
+
     @twilio_client.account.sms.messages.create(
       :from => "+1#{twilio_phone_number}",
       :to => number_to_send_to,
-      :body => "This is an message. It gets sent to #{number_to_send_to}"
+      :body => "There will be street cleaning in your ward soon. Please move your car. "
     )
   end
 end
+
