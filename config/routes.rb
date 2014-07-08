84e41ff62b606eaa514eb50469248ef074b7b54f
pdebelak <pdebelak@gmail.com>
2014-07-08 13:53:40 -0500

    Forgot password now redirects to root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users, controllers: { sessions: 'sessions'}
+  devise_for :users, controllers: { :passwords => "passwords" }
   resources :users, only: [:show]
   resources :notifications, only: [:create, :destroy]
 
