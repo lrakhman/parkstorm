90719e485e73cf28f388f72e61e76d15f45f6c89
lrakhman <lana.rakhman@gmail.com>
2014-07-09 21:07:25 -0500

    deleted files

--- app/controllers/receive_text_controller.rb
+++ app/controllers/receive_text_controller.rb
@@ -1,8 +1,6 @@
 class ReceiveTextController < ApplicationController
 	def index 
-    # let's pretend that we've mapped this action to 
-    # http://localhost:3000/sms in the routes.rb file
-    
+   
     message_body = params["Body"]
     from_number = params["From"]
  
