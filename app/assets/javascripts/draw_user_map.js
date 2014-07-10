96a4298d0dd7535a56f847a4d722efa278108539
NatalieFrecka <nfrecka@gmail.com>
2014-07-09 19:23:08 -0500

    user map is up

--- app/assets/javascripts/draw_user_map.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-  $.post('/load_user_map', {date: getDateRange()}, function(response) {
-    $('#personal_map').html(response);
-  });
-});
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,23 +12,19 @@ class UsersController < ApplicationController
 
   def show
     if current_user
+      @user = User.find_by_id(params[:id])
+      @user.find_user_notifications
+      @regions = @user.get_user_regions
       redirect_to root_path unless current_user.id == params[:id].to_i
     else
       redirect_to root_path
     end
-    @user = User.find_by_id(params[:id])
-    @user.find_user_notifications
-    @regions = @user.get_user_regions
+
   end
 
   def date_range
     @today = Date.today
     @week_from_today = Date.today + 7
-  end
-
-  def load_user_map
-    @user = current_user
-    render partial: 'user_map', locals: {regions: @user.get_user_regions}
   end
 end
 
